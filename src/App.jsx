afb8d355348e6b570c9b4ab7fca68e66851b3ae1
Your Nameleo-moon <lvv0820@gmail.com>
2026-05-18 18:01:11 +0300

    function Learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header.jsx';
+import { ports } from './data';
+
+function Learning(props) {
+  console.log(props);
+  return (
+    <li>
+      <p>
+        <strong>1 row</strong>1 row coment
+      </p>
+    </li>
+  );
+}
 
 export default function App() {
   return (
@@ -8,7 +20,14 @@ export default function App() {
       <Header />
       <main>
         <h1>Hello react</h1>
-      </main>{' '}
+        <section>
+          <h3>Start list</h3>
+          <ul>
+            <Learning title="11" deacription="22" />
+            <Learning title="3311" deacription="4422" />
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const ports = [
+  {
+    title: '1111111',
+    description: '1111111aaaaa',
+  },
+  {
+    title: '222222',
+    description: '222222222aaaaa',
+  },
+  {
+    title: '333333',
+    description: '33333333aaaaa',
+  },
+];
