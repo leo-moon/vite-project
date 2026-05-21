9ffb325f3b63e086194aa3b53f73a9875dcb1f14
leo-moon <lvv0820@gmail.com>
2026-05-21 15:25:46 +0300

    20260521 test

--- src/data.js
+++ src/data.js
@@ -5,10 +5,16 @@ export const ports = [
   },
   {
     title: '222222',
-    description: '222222222aaaaa',
+    description: '2222222aaaaa',
   },
   {
     title: '333333',
-    description: '33333333aaaaa',
+    description: '3333333aaaaa',
   },
 ];
+
+export const differences = {
+  way: 'Различия между React и Vue',
+  easy: 'Легкость изучения и использования',
+  hard: 'Сложность настройки и оптимизации',
+};
